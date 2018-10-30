6457f9148225aebae64e65e0ff0678bd0d46a091
PROFILE\jasiabra <joaquin.siabra@gmail.com>
2018-10-30 17:22:07 +0100

    Adaptación del diccionario

--- src/app/mock-dict.ts
+++ src/app/mock-dict.ts
@@ -1,21 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HashTable } from 'angular-hashtable';
 
 @Injectable()
 export class DiccionarioService {
 
-  dict: any[] = [];
+  dict: any = null;
+  table: HashTable<string, any>;
 
   constructor(private http: HttpClient) {
-      this.http.get<any[]>('./../../assets/salida.json').subscribe(
+      this.http.get<any>('./../../assets/salida.json').subscribe(
         data => {
             this.dict = data;
-            console.log(this.dict);
+            //console.log(this.dict);
+            //console.log(this.dict.items.item);
+            this.table = new HashTable<string, any>();
+
+            for (let w of this.dict.items.item) {
+                this.table.put(w.word, {
+                    trad: w.trads? w.trads.value.lemma_definition: null,
+                    link: w.link
+                });
+                console.log(this.table.get(w.word)); 
+            }
+
         }
     );
   }
 
-  getAll(): any[] {
+  getAll(): any {
     return this.dict;
   }
 }
